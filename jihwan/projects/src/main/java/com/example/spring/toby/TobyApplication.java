8ae187d2959bb6abd56a2ccf9014707b044ee903
jihwooon <jihwooon@gmail.com>
2023-06-08 17:07:23 +0900

    main을 이용한 DAO 테스트를 하라
    DAO를 호출해서 User 정보를 DB에 저장하는 테스트를 작성했습니다.

--- jihwan/projects/src/main/java/com/example/spring/toby/TobyApplication.java
+++ jihwan/projects/src/main/java/com/example/spring/toby/TobyApplication.java
@@ -1,13 +1,26 @@
 package com.example.spring.toby;
 
+import com.example.spring.toby.users.User;
+import com.example.spring.toby.users.UserDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class TobyApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		SpringApplication.run(TobyApplication.class, args);
+
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("1L");
+		user.setName("토비");
+		user.setPassword("1234");
+
+		dao.add(user);
 	}
 
 }
